2ad53c4654338c9f1932b43762126b2e3092559b
Johannes Edmeier <johannes.edmeier@steadybit.com>
2025-10-30 22:29:13 +0100

    test: add missing connmark module

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -1280,6 +1280,7 @@ func getMinikubeOptions() e2e.MinikubeOpts {
 		log.Info().Msg("KVM is available, using kvm2 driver")
 		mOpts = mOpts.WithDriver("kvm2").
 			AfterStart(func(m *e2e.Minikube) error {
+				log.Info().Msg("Installing xt_connmark module")
 				return m.SshExec("sudo", "modprobe", "xt_connmark").Run()
 			})
 	}
