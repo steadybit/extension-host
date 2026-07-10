2aa9c8021b5b1287e0e7af833b038b14b78658ae
Johannes Edmeier <johannes.edmeier@steadybit.com>
2026-07-11 00:11:52 +0200

    refactor: register extension index via exthttp.RegisterRevisionedHandler
    Replace the hand-rolled startedAt + IfNoneMatchHandler index wiring with
    exthttp.RegisterRevisionedHandler, so the index ETag is centralized in the SDK
    and reflects registration state instead of process start. Bumps extension-kit
    to v1.11.0 and the SDK kits to the releases that bump the revision on
    register/clear.

--- main.go
+++ main.go
@@ -4,8 +4,6 @@
 package main
 
 import (
-	"time"
-
 	_ "github.com/KimMachineGun/automemlimit" // By default, it sets `GOMEMLIMIT` to 90% of cgroup's memory limit.
 	"github.com/rs/zerolog"
 	"github.com/steadybit/action-kit/go/action_kit_api/v2"
@@ -23,8 +21,6 @@ import (
 	"github.com/steadybit/extension-kit/extruntime"
 	"github.com/steadybit/extension-kit/extsignals"
 )
-
-var startedAt = time.Now().Format(time.RFC3339)
 
 func main() {
 	// Most Steadybit extensions leverage zerolog. To encourage persistent logging setups across extensions,
@@ -84,7 +80,7 @@ func main() {
 	action_kit_sdk.RegisterAction(exthost.NewFillDiskHostAction(r))
 	action_kit_sdk.RegisterAction(exthost.NewFillMemoryHostAction(r))
 
-	exthttp.RegisterHttpHandler("/", exthttp.IfNoneMatchHandler(func() string { return startedAt }, exthttp.GetterAsHandler(getExtensionList)))
+	exthttp.RegisterRevisionedHandler("/", getExtensionList)
 
 	//This will install a signal handler, that will stop active actions when receiving a SIGURS1, SIGTERM or SIGINT
 	extsignals.ActivateSignalHandlers()
